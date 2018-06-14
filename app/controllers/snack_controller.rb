3e2db7061a311fcdb69e782969147bd7eae8923e
Cameron Watts <cqwatts@cps.edu>
2018-06-14 13:09:26 +0000

    Add table and more css

--- app/controllers/snack_controller.rb
+++ app/controllers/snack_controller.rb
@@ -1,12 +1,37 @@
 class SnackController < ApplicationController
     def index
+        @order = Order.all
     end
     
     def show
+        @order = Order.find_by_id(params[:id])
     end
     
     def create
-        
+       o = Order.new
+       o.customer_name = params['name']
+       o.location = params['location']
+       o.description = params['description']
+       o.save
+       redirect_to "/snack/show/#{ o.id }"
+    end
+    
+    def edit
+        @order = Order.find_by_id(params[:id])
     end
 
+def update
+       o = Order.new
+       o.customer_name = params['name']
+       o.location = params['location']
+       o.description = params['description']
+       o.save
+       redirect_to "/snack/show/#{ o.id }"
+end
+
+    def destroy
+    o = Order.find_by_id(params[:id])
+    o.destroy
+    redirect_to "/snack/index"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'snack/index', to: 'snack#index'
-  get 'snack/show', to: 'snack#show'
-  get 'snack/new', to: 'snack#new'
-  get 'snack/create', to: 'snack#create'
+  get 'snack/index', to: 'snack#new'
+  get 'snack/show/:id', to: 'snack#show'
+  get 'snack/data', to: 'snack#index'
+  get '/snack/snack/create', to: 'snack#create'
+  get 'snack/edit/:id', to: 'snack#edit'
+  get 'snack/update/:id', to: 'snack#update'
+  get 'snack/destroy/:id', to: 'snack#destroy'
 end
